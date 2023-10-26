b0aacd2189e396129383f64bdbaed48065b2065a
tweppy <tweppy@gmail.com>
2023-10-26 11:35:18 +0200

    update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -24,7 +24,7 @@ export async function submitReview(data, meetupId) {
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlIwdXljY0YzRkhmb19qRkdIS3ZFTSIsImlhdCI6MTY5ODE0OTY2MywiZXhwIjoxNjk4MTUzMjYzfQ.SbZt1L-tm8hGNSyajXF7qQNe8HSKxFwKMpLHF8MpBDY';
 
   try {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/meetups/reviews/${meetupId}`, {
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}meetups/reviews/${meetupId}`, {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${token}`,
