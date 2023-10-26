24b685320612287985cfd6f37a87761b045030fe
Fredrik <io.qrdr@gmail.com>
2023-10-26 11:26:20 +0200

    fix string

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -24,17 +24,14 @@ export async function submitReview(data, meetupId) {
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IlIwdXljY0YzRkhmb19qRkdIS3ZFTSIsImlhdCI6MTY5ODE0OTY2MywiZXhwIjoxNjk4MTUzMjYzfQ.SbZt1L-tm8hGNSyajXF7qQNe8HSKxFwKMpLHF8MpBDY';
 
   try {
-    const response = await fetch(
-      `${import.meta.env.VITE_BASE_URL}/api/meetups/reviews/${meetupId}`,
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/meetups/reviews/${meetupId}`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify(data),
+    });
 
     const result = await response.json();
     return result;
