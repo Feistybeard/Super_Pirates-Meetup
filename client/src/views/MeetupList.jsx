cef3a156fde60a86281b85b21b761aa3c2656fb5
NguyenRobin <robin.nnguyen@hotmail.com>
2023-10-26 13:05:51 +0200

    feature: searchInput field for keywords

--- client/src/views/MeetupList.jsx
+++ client/src/views/MeetupList.jsx
@@ -1,60 +1,59 @@
 import { useEffect, useState } from 'react';
-import { getMeetups, getSearchResult } from '../utils/api';
+import { getMeetups } from '../utils/api';
 import Meetup from '../components/Meetup/Meetup';
+import SearchInput from '../components/SearchInput/SearchInput';
 
 function MeetupList() {
   const [meetups, setMeetups] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
   const [search, setSearch] = useState('');
+
+  function getMeetupsByQuery(queryStr) {
+    const words = queryStr.toLowerCase().split(' ');
+    const findMatch = meetups.filter((meetup) => {
+      for (const word of words) {
+        if (meetup.keywords.includes(word)) {
+          return meetup;
+        }
+      }
+    });
+    return findMatch;
+  }
 
   useEffect(() => {
     async function fetchData() {
       const data = await getMeetups();
       setMeetups(data.meetups);
+      setSearchResult(data.meetups);
     }
     fetchData();
   }, []);
 
   function handleOnChange(e) {
-    console.log(e.target.value);
     setSearch(e.target.value);
   }
 
   async function handleClick() {
-    const test = await getSearchResult(search);
-    console.log(test);
+    if (search === '' || search.length < 2) return;
+    const query = await getMeetupsByQuery(search);
+    setSearchResult(query);
+    setSearch('');
   }
 
   return (
     <div className='flex flex-col mt-20 '>
-      <div className='join justify-center fixed'>
-        <div>
-          <div>
-            <input
-              className='input input-bordered join-item'
-              placeholder='Search'
-              onChange={handleOnChange}
-            />
-          </div>
-        </div>
-        <select className='select select-bordered join-item'>
-          <option disabled>Filter</option>
-          <option>Sci-fi</option>
-          <option>Drama</option>
-          <option>Action</option>
-        </select>
-        <div className='indicator'>
-          <button className='btn join-item' onClick={handleClick}>
-            Search
-          </button>
-        </div>
-      </div>
+      <SearchInput onChange={handleOnChange} value={search} onClick={handleClick} />
 
       <div className='flex justify-center'>
-        <ul className='grid grid-cols-4 gap-4 bg-base-100 shadow-xl '>
-          {meetups.map((meetup) => (
-            <Meetup key={meetup.id} meetup={meetup} />
-          ))}
-        </ul>
+        {!searchResult.length ? (
+          <p className='mt-10'>No results found...</p>
+        ) : (
+          <ul className='grid grid-cols-4 gap-4 bg-base-100 shadow-xl '>
+            {searchResult.map((meetup) => (
+              <Meetup key={meetup.id} meetup={meetup} />
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
