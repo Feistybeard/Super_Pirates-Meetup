e5661069cf81c6db6191af097a153b53d3a9b3b0
Fredrik <io.qrdr@gmail.com>
2023-10-30 10:20:12 +0100

    restyle

--- client/src/components/Reviews/ListReviews/ListReviews.jsx
+++ client/src/components/Reviews/ListReviews/ListReviews.jsx
@@ -7,19 +7,22 @@ export default function ListReviews({ reviews, maxScore = 5 }) {
       {reviews.map((review, index) => (
         <Fragment key={review.id}>
           <article className='card bg-base-300 rounded-box'>
-            <div className='card-body'>
-              <header className='flex flex-row'>
-                <AiOutlineUser size={28} />
-                <h2 className='card-title pl-2'>{review.username}</h2>
-              </header>
+            <div className='card-body flex flex-row gap-4'>
+              <aside>
+                <header className='flex flex-row'>
+                  <AiOutlineUser size={28} />
+                  <h2 className='card-title pl-2'>{review.username}</h2>
+                </header>
+              </aside>
 
-              <section className='flex flex-row'>
-                {Array.from({ length: maxScore }, (_, number) => number + 1).map((index) => (
-                  <AiFillStar fill={index <= review.score ? 'yellow' : 'gray'} key={index} />
-                ))}
-              </section>
-
-              <p className='text-start italic'>"{review.comment}"</p>
+              <aside className='border-l border-neutral pl-4 flex flex-col'>
+                <p className='text-start italic'>"{review.comment}"</p>
+                <section className='flex flex-row pt-2'>
+                  {Array.from({ length: maxScore }, (_, number) => number + 1).map((index) => (
+                    <AiFillStar fill={index <= review.score ? 'yellow' : 'gray'} key={index} />
+                  ))}
+                </section>
+              </aside>
             </div>
           </article>
           {reviews.length > 1 && reviews.length - 1 !== index && <div className='divider'></div>}
